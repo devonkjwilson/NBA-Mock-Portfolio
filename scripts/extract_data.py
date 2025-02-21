9c5d8b53a809cac17768397bb39c7a7c19cd06e9
devonkjwilson <devonkjwilson@aol.com>
2025-02-20 22:47:28 -0600

    Update extract_data.py

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 # Define file path
 Players_path = "data/Players.csv"  # Update the path if needed
-
+LeaugeSchedule_path = "data/LeagueSchedule24_25.csv"
+Playerstats_path = "data/PlayerStatistics (1).xlsx"
+TeamHistories_path "data/TeamHistories.csv"
+TeamStats_path = "data/TeamStatistics.csv"
 try:
     # Load CSV file
     df = pd.read_csv(Players_path)
@@ -10,5 +13,44 @@ try:
     print(df.head())  # Show first 5 rows
 except FileNotFoundError:
     print(f"❌ Error: File {Players_path} not found.")
+except Exception as e:
+    print(f"❌ Error: {e}")
+try:
+    # Load CSV file
+    df = pd.read_csv(LeaugeSchedule_path)
+    print(f"✅ Successfully extracted data from {LeaugeSchedule_path}")
+    print(df.head())  # Show first 5 rows
+except FileNotFoundError:
+    print(f"❌ Error: File {LeaugeSchedule_path} not found.")
+except Exception as e:
+    print(f"❌ Error: {e}")
+
+try:
+    # Load CSV file
+    df = pd.read_csv(Playerstats_path)
+    print(f"✅ Successfully extracted data from {Playerstats_path}")
+    print(df.head())  # Show first 5 rows
+except FileNotFoundError:
+    print(f"❌ Error: File {Playerstats_path} not found.")
+except Exception as e:
+    print(f"❌ Error: {e}")
+
+try:
+    # Load CSV file
+    df = pd.read_csv(TeamHistories_path)
+    print(f"✅ Successfully extracted data from {TeamHistories_path}")
+    print(df.head())  # Show first 5 rows
+except FileNotFoundError:
+    print(f"❌ Error: File {TeamHistories_path} not found.")
+except Exception as e:
+    print(f"❌ Error: {e}")
+
+try:
+    # Load CSV file
+    df = pd.read_csv(TTeamStats_path)
+    print(f"✅ Successfully extracted data from {TeamStats_path}")
+    print(df.head())  # Show first 5 rows
+except FileNotFoundError:
+    print(f"❌ Error: File {TeamStats_path} not found.")
 except Exception as e:
     print(f"❌ Error: {e}")
